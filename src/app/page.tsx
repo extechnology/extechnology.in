206f68bb933d686e080123eb21308ce343f74b58
VIPINRAJK26 <vipinrajk026@gmail.com>
2025-03-06 10:58:24 +0530

    modified page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/ui/Home/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-zinc-800">
       <div>
         <Hero/>
       </div>
--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -104,10 +104,10 @@ export const HeroParallax = ({
 export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-8xl font-bold dark:text-white">
+      <h1 className="text-2xl md:text-8xl font-bold text-white">
         Extechnology
       </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+      <p className="max-w-2xl text-base md:text-xl mt-8 text-neutral-200">
         Extechnology offering is dedicated to delivering advanced IT solutions,
         including web development, e-commerce platforms, Web & WhatsApp
         Chatbots, and Mobile applications, ensuring comprehensive support of our
